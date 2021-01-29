80797b4d71ed2cb418c0c4eaee203562cc41892a
dhanidev <husein@asiansigmatechnology.com>
2021-01-29 21:51:24 +0700

    integration with git

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ Route::middleware('guest')->group(function () {
     Route::post('register', 'AuthController@register')->name('register');
     Route::post('login', 'AuthController@login')->name('login');
     Route::post('refresh-token', 'AuthController@refreshToken')->name('refreshToken');
+
+    Route::get('status', 'AuthController@check')->name('check status');
 });
 
 Route::middleware('auth:api')->group(function () {
